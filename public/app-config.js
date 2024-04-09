605768bb87908cd6ff2391743a9dce02ee3e4d27
Dmitry <kuznetsov2d@gmail.com>
2024-04-08 19:14:27 -0700

    Updated translation source language in app-config.js

--- public/app-config.js
+++ public/app-config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
     CONFIG_OPENAI_URL: 'https://api.openai.com/v1/chat/completions',
     CONFIG_OPENAI_TOKEN: '',
     CONFIG_OPENAI_MODEL: 'gpt-4',
-    CONFIG_TRANSLATE_FROM: 'Russian'
+    CONFIG_TRANSLATE_FROM: ''
 };
